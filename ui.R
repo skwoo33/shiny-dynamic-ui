7f7489e1f2c77bf980e139165da6acd7c64c6a9e
skwoo33 <sangkyu.woo33@gmail.com>
2017-04-14 06:42:26 +0900

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,31 @@
+fluidPage(
+  titlePanel("Dynamically generated user interface components"),
+  fluidRow(
+    column(3, wellPanel(
+      selectInput("input_type", "Input type",
+        c("slider", "text", "numeric", "checkbox",
+          "checkboxGroup", "radioButtons", "selectInput",
+          "selectInput (multi)", "date", "daterange"
+        )
+      )
+    )),
+    # column(3,
+    #   selectInput("input_type", "Input type",
+    #     c("slider", "text", "numeric", "checkbox",
+    #       "checkboxGroup", "radioButtons", "selectInput",
+    #       "selectInput (multi)", "date", "daterange"
+    #     )
+    #   )
+    # ),
+    column(3, wellPanel(
+      # uiOutput("ui")
+      htmlOutput("ui")
+    )),
+    column(3,
+      tags$p("Input type:"),
+      verbatimTextOutput("input_type_text"),
+      tags$p("Dynamic input value:"),
+      verbatimTextOutput("dynamic_value")
+    )
+  )
+)
